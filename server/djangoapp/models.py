e52ed4db4d0fedb7f136e0fb6ea0bbb3fb3d6117
rubasankar <rubasankar2002@gmail.com>
2024-05-06 20:07:21 +0530

    week-3v2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -37,7 +37,7 @@ class CarModel(models.Model):
         ('SUV','SUV'),
         ('WAGON','Wagon'),
     ]
-    cartype = models.CharField(max_length=10,choices=CAR_TYPES,default='SUV')
+    type = models.CharField(max_length=10,choices=CAR_TYPES,default='SUV')
     year = models.IntegerField(default=2023,
                                validators=[
                                    MaxValueValidator(2023),
